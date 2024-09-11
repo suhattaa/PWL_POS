b55ceb93d01706ff531d2a1dd7ed203a07a58c01
Suhatta <suhatta26@gmail.com>
2024-09-11 22:06:09 +0700

    JS03_Praktikum4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LevelContoller;
 use Illuminate\Support\Facades\Route;
+use Monolog\Level;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/level', [LevelContoller::class, 'index']);
