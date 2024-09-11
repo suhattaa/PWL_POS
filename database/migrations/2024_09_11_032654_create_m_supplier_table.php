8ba3fe2226eb2cfb02167222dc86e816c6834a89
Suhatta <suhatta26@gmail.com>
2024-09-11 18:38:11 +0700

    JS03_Praktikum2

--- database/migrations/2024_09_11_032654_create_m_supplier_table.php
+++ database/migrations/2024_09_11_032654_create_m_supplier_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_supplier', function (Blueprint $table) {
-            $table->id();
+            $table->id('supplier_id');  
             $table->string('supplier_kode', 10);
             $table->string('supplier_nama', 100);
             $table->string('supplier_alamat', 255);
-            $table->timestamps();
+    $table->timestamps();
         });
     }
 
