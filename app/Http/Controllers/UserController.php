8765cd4673750fab7082f97f5be020fdfe33fe68
Suhatta <suhatta26@gmail.com>
2024-10-05 16:05:50 +0700

    JS06_Praktikum3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -260,4 +260,29 @@ class UserController extends Controller
             return redirect('/user')->with('error', 'Data User Gagal dihapus karena terdapat Tabel lain yang terkait dengan data ini');
         }
     }
+    public function confirm_ajax(string $id)
+    {
+        $user = UserModel::find($id);
+        return view('user.confirm_ajax', ['user' => $user]);
+    }
+    public function delete_ajax(Request $request, $id) 
+    {
+        if($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        } else {
+            return redirect('/');
+        }
+    }
 }
